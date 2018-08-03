d1822f8da7c42cf4b6fe6dcae8cb0dc9a438614b
trungh13 <newbie.trungh13@gmail.com>
2018-08-03 17:13:59 +0300

    add Mailer

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,5 +16,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-
+const Mailer = require('../services/Mailer');
 const Survey = mongoose.model('surveys');
-
+const surveyTemplate = require('../services/emailTemplate/surveyTemplate.js');
 module.exports = (app) => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', (req, res) => {
     const { title, subject, body, recipients } = req.body;
+    console.log(req.user)
     const survey = new Survey({
       title,
       body,
       subject,
       recipients: recipients.split(',').map((email) => ({ email: email.trim() })),
-      _user: req.user.id,
+      //_user: req.user.id,
       dateSent: Date.now(),
       lastResponded: Date
     });
+    //Send mail here
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    console.log('mailer.send');
+    mailer.send();
   });
 };
